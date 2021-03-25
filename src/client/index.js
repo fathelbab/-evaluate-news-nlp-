a2e1803a923269bcfdc924f20bec046ae39f6e69
Mohamed Ahmed Fathelbab <fat7isoft@gmail.com>
2021-03-25 15:17:28 +0200

    finalizing the project

---
+++ src/client/index.js
@@ -0,0 +1,11 @@
+/*
+author mohamed ahmed fathelbab
+*/
+
+
+import { validateUrl } from './js/urlValidation'
+import {onPressHandler} from './js/formHandler'
+import './styles/styles.scss'
+import './styles/resets.scss'
+
+onPressHandler()
--- webpack.prod.js
+++ webpack.prod.js
@@ -3,7 +3,9 @@ const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
 const Dotenv = require('dotenv-webpack');
 const WorkboxPlugin = require('workbox-webpack-plugin');
-
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 module.exports = {
     entry: './src/client/index.js',
     mode: 'production',
@@ -16,9 +18,9 @@ module.exports = {
             },
             {
                 test: /\.scss$/,
-                use: [ 'style-loader', 'css-loader', 'sass-loader' ]
+                use: [ MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader' ]
             }
-        ]
+        
         ]
     },
     plugins: [
@@ -26,6 +28,7 @@ module.exports = {
             template: "./src/client/views/index.html",
             filename: "./index.html",
         }),
+        new MiniCssExtractPlugin({ filename: '[name].[contenthash].css' }),
         new WorkboxPlugin.GenerateSW()
     ]
 }
